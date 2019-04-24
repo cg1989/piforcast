a1d3771a4cc9193ef145e110331257d9d85b78d2
cg1989 <camille.geimer@hotmail.fr>
2019-04-24 15:37:43 +0200

    nettoyage du projet

--- src/CMakeFiles/piforcast_automoc.dir/AutomocOldMocDefinitions.cmake
+++
@@ -1,1 +0,0 @@
-set(AM_OLD_COMPILE_SETTINGS "QT_CORE_LIB;QT_GUI_LIB;QT_NETWORK_LIB;QT_NO_DEBUG;QT_QML_LIB ~~~ /usr/include/arm-linux-gnueabihf/qt5;/usr/include/arm-linux-gnueabihf/qt5/QtQml;/usr/include/arm-linux-gnueabihf/qt5/QtNetwork;/usr/include/arm-linux-gnueabihf/qt5/QtCore;/usr/lib/arm-linux-gnueabihf/qt5/mkspecs/linux-g++;/usr/include/arm-linux-gnueabihf/qt5/QtGui ~~~  ~~~ FALSE ~~~ ")
