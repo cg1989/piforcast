92deedf11ad1f99e1146921d3fe6db6fee324586
cg1989 <camille.geimer@hotmail.fr>
2019-04-30 14:29:25 +0200

    Finalisation de la doc

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
 #include "sensors.h"
 #include "bme280.h"
 
+/** \mainpage
+ * 
+ *
+ * Ce logiciel est fourni sous une licence GNU GPL. Toutes redistributions du code doivent être fournies avec les possibles modifications lui ayant été appliquées.
+ *
+ * Les sources du projet Pi4cast sont disponible à l'adresse suivante: https://github.com/cg1989/piforcast.git
+ *
+ * 
+ * File		main.cpp
+ * Date		30 avr 2019
+ * Version	1.0
+ *
+ */
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
